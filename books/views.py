43c1c7518314a59f7fdc2cd78d65d6e8985a3e33
Lukasrid <lukas.ridstrom@gmail.com>
2023-09-12 09:13:33 +0000

    added sorting functionality

--- books/views.py
+++ books/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Book
+from .models import Book, Genre
 
 # Create your views here.
 
@@ -10,20 +10,46 @@ def all_books(request):
 
     books = Book.objects.all()
     query = None
+    genres = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                books = books.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            books = books.order_by(sortkey)
+            
+
+        if 'genre' in request.GET:
+            genres = request.GET['genre'].split(',')
+            books = books.filter(genre__friendly_name__in=genres)
+            genres = Genre.objects.filter(friendly_name__in=genres)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('books'))
             
-            queries = Q(title__icontains=query) | Q(summary__icontains=query)
+            queries = Q(title__icontains=query) | Q(summary__icontains=query) | Q(author__icontains=query)
             books = books.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'books': books,
         'search_term': query,
+        'current_genres': genres,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'books/books.html', context)
