afc9d2d74b82e364910b754e89a8896685cb1008
Lukasrid <lukas.ridstrom@gmail.com>
2023-09-21 06:52:03 +0000

    Product form and admin

---
+++ books/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Book, Genre
+
+
+class BookForm(forms.ModelForm):
+
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        genres = Genre.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in genres]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
