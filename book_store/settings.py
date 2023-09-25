a0fe3ee26d649cd4e0871cc91640f44df4e332cb
Lukasrid <lukas.ridstrom@gmail.com>
2023-09-25 13:26:01 +0000

    Update debug to false

--- book_store/settings.py
+++ book_store/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environment
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-lukasrid-ecommercebooks-xvhi321oj2k.ws-eu104.gitpod.io', 'book-store-lukarid-9168f1b715ae.herokuapp.com', 'localhost']
 
